e31ca6421bf778fe9ce93440f1e5e58000c59953
shmen-shmen <akshevchenko@proton.me>
2023-04-18 13:16:39 +0600

    weather looks nicer, log saves on button click

--- public/sketch.js
+++ public/sketch.js
@@ -1,17 +1,22 @@
 const latitudeEl = document.getElementById("lat");
 const longitudeEl = document.getElementById("lon");
+const waitEl = document.getElementById("wait");
 
 if ("geolocation" in navigator) {
 	console.log("GEOLOCATION IS AVAILABLE");
 
 	navigator.geolocation.getCurrentPosition(async (position) => {
+		// FIRST WE GET CLIENT COORDINATES
 		const myLat = position.coords.latitude;
 		const myLon = position.coords.longitude;
 
+		// SEND COORDS TO SERVER TO MAKE API CALL
 		const apiURL = `/weather/${myLat}-${myLon}`;
 		const weather_response = await fetch(apiURL);
+		// GET WEATHER DATA
 		const weather_data = await weather_response.json();
 
+		// AND TURN IT INTO A WEATHER REPORT IN DOM
 		const cityEl = document.getElementById("city");
 		cityEl.innerText = weather_data["name"];
 
@@ -37,25 +42,31 @@ if ("geolocation" in navigator) {
 		const windEl = document.getElementById("wind");
 		windEl.innerText = weather_data["wind"]["speed"];
 
-		const waitEl = document.getElementById("wait");
+		// WHEN ALL THE DOM IS READY THEN "PLEASE WAIT" MESSAGE DISSAPPERARS
+		// AND WEATHER REPORT IS SHOWN
 		waitEl.style.display = "none";
 		const weatherWrapperEl = document.getElementById("weather-wrapper");
 		weatherWrapperEl.style.display = "block";
 
-		const data = { myLat, myLon };
-		const options = {
-			method: "POST",
-			body: JSON.stringify(data),
-			headers: { "Content-Type": "application/json" },
+		// YOU CAN LOG A WEATHER REPORT UPON BUTTON CLICK
+		const saveLogBtn = document.getElementById("save-log");
+		const saveLogFunc = async () => {
+			const data = { myLat, myLon };
+			const options = {
+				method: "POST",
+				body: JSON.stringify(data),
+				headers: { "Content-Type": "application/json" },
+			};
+			const response = await fetch("/api", options);
+			console.log("COORDINATES SENT TO SERVER, AWAIT RESPONSE");
+			const newData = await response.json();
+			console.log("RESPONSE FROM SERVER: ", newData);
 		};
-		const response = await fetch("/api", options);
-		console.log("COORDINATES SENT TO SERVER, AWAIT RESPONSE");
-		const newData = await response.json();
-		console.log("RESPONSE FROM SERVER: ", newData);
+		saveLogBtn.addEventListener("click", saveLogFunc);
 	});
 } else {
 	console.log("ERROR GEOLOCATION IS NOT AVAILABLE");
-	latitudeEl.innerText = "*******";
-	longitudeEl.innerText = "*******";
-	sendBtnEl.innerText = "GEO DATA NOT AVAILABLE";
+	waitEl.style.display = "block";
+	waitEl.innerText =
+		"Sorry could not get your weather because geolocation for your device is uavailable.";
 }

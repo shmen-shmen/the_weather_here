647157eb26611c269b27d19e6eb66529dfcccb7d
shmen-shmen <akshevchenko@proton.me>
2023-04-21 13:39:09 +0600

    styles stuff

--- public/sketch.js
+++ public/sketch.js
@@ -13,6 +13,8 @@ if ("geolocation" in navigator) {
 		// FIRST WE GET CLIENT COORDINATES
 		const myLat = position.coords.latitude;
 		const myLon = position.coords.longitude;
+		// LOAD MAP WITH CLIENT COORDINATES
+		map.setView([myLat, myLon], 15);
 		try {
 			// SEND COORDS TO SERVER TO MAKE API CALL
 			const apiURL = `/weather/${myLat}-${myLon}`;
@@ -51,8 +53,7 @@ if ("geolocation" in navigator) {
 			const weatherReportEl = document.getElementById("ready");
 			weatherReportEl.style.display = "block";
 
-			// LOAD MAP WITH CLIENT COORDINATES AND ADD MARKER
-			map.setView([myLat, myLon], 15);
+			// ADD MARKER TO MAP
 			let marker = L.marker([myLat, myLon]).addTo(map);
 			marker
 				.bindPopup(
@@ -85,5 +86,49 @@ if ("geolocation" in navigator) {
 	console.log("ERROR GEOLOCATION IS NOT AVAILABLE");
 	waitEl.style.display = "block";
 	waitEl.innerText =
-		"Sorry could not get your weather because geolocation for your device is uavailable.";
+		"Sorry could not get your weather because geolocation for your device is uavailable 🥲.";
+}
+
+// ***
+// CODE TO DRAG WEATHER REPORT AROUND THE PAGE
+// ***
+dragElement(document.getElementById("drag"));
+
+function dragElement(elmnt) {
+	var pos1 = 0,
+		pos2 = 0,
+		pos3 = 0,
+		pos4 = 0;
+
+	elmnt.onmousedown = dragMouseDown;
+
+	function dragMouseDown(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// get the mouse cursor position at startup:
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		// call a function whenever the cursor moves:
+		document.onmousemove = elementDrag;
+	}
+
+	function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+		// calculate the new cursor position:
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		// set the element's new position:
+		elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+		elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+	}
+
+	function closeDragElement() {
+		// stop moving when mouse button is released:
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
 }
